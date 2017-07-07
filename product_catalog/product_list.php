bfd780c55886d9dd7a1b3db9501077d01323501a
sa947 <sa947@njit.edu>
2017-07-07 19:35:56 -0400

    committed catalog changes

--- product_catalog/product_list.php
+++ product_catalog/product_list.php
@@ -2,63 +2,36 @@
 <main>
     <aside>
             <h1>Categories</h1>
-	            <nav>
-		            <ul>
-			            
-				                <?php foreach($categories as
-						$category) : ?>
-						            <li>
-							                    <a
-									    href="?category_id=<?php
-									    echo
-									    $category['categoryID'];
-									    ?>">
-									                        <?php
-												echo
-												$category['categoryName'];
-												?>
-												                </a>
-														            </li>
-															                <?php
-																	endforeach;
-																	?>
-																	        </ul>
-																		        </nav>
-																			    </aside>
-																			        <section>
-																				        <h1><?php
-																					echo
-																					$category_name;
-																					?></h1>
-																					        <ul
-																						class="nav">
-																						        
-																							            <?php
-																								    foreach
-																								    ($products
-																								    as
-																								    $product)
-																								    :
-																								    ?>
-																								                <li>
-																										                <a
-																												href="?action=view_product&amp;product_id=<?php 
-																												                          echo
-																															  $product['productID'];
-																															  ?>">
-																															                      <?php
-																																	      echo
-																																	      $product['productName'];
-																																	      ?>
-																																	                      </a>
-																																			                  </li>
-																																					              <?php
-																																						      endforeach;
-																																						      ?>
-																																						              </ul>
-																																							          </section>
-																																								  </main>
-																																								  <?php
-																																								  include
-																																								  '../view/footer.php';
-																																								  ?>
+	           <?php include '../view/category_nav.php'; ?>
+		       </aside>
+		           <section>
+			           <h1><?php echo $category_name; ?></h1>
+				           <ul class="nav">
+					             
+						                 <?php
+								 foreach
+								 ($products
+								 as $product)
+								 : ?>
+								             <li>
+									                     <a
+											     href="?action=view_product&amp;product_id=<?php 
+											                               echo
+														       $product['productID'];
+														       ?>">
+														                           <?php
+																	   echo
+																	   $product['productName'];
+																	   ?>
+																	                   </a>
+																			               </li>
+																				                   <?php
+																						   endforeach;
+																						   ?>
+																						           </ul>
+																							       </section>
+																							       </main>
+																							       <?php
+																							       include
+																							       '../view/footer.php';
+																							       ?>
